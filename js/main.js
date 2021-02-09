1c56907aa7281fad84e549eae3429a73b700508b
Valerio <valeriodevivo90@gmail.com>
2021-02-09 17:27:38 +0100

    Finish

--- js/main.js
+++ js/main.js
@@ -1,13 +1,26 @@
 var app = new Vue({
 	el:"#app",
 	data: {
-		cds: ''
+		cds: '',
+		genere:['all'],
+		opt: 'all'
 	},
 	mounted() {
 		axios
 			.get("https://flynn.boolean.careers/exercises/api/array/music")
 			.then((result) => {
 				this.cds = result.data.response;
-			})
+				
+				for (let i = 0; i < this.cds.length; i++) {
+					if (!this.genere.includes(this.cds[i].genre)) {
+						this.genere.push(this.cds[i].genre);
+					}
+				}
+			});
+	},
+	methods: {
+		optlog(i){
+			this.opt = this.genere[i];
+		}
 	}
 });
